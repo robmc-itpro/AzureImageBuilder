828612e2dfa114cb39f89919a586872ddda52168
RobMc-ITPro <robert.mcarthur@consilium-uk.com>
2020-05-08 13:14:32 +0100

    commit to master

--- Scripts/Create-Image.ps1
+++ Scripts/Create-Image.ps1
@@ -1,3 +1,7 @@
 # Start image creation
 
 Invoke-AzResourceAction -ResourceName $imageTemplateName -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ApiVersion "2019-05-01-preview" -Action Run -Force
+
+# Check Status
+
+(Get-AzResource -ResourceGroupName RG_NEU_AzureImageBuilder -ResourceType Microsoft.VirtualMachineImages/imageTemplates -Name $ImageTemplateName).Properties.lastRunStatus

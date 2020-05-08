b6a47fd13525d084fa2760aa882cd955f41ac9fb
RobMc-ITPro <robert.mcarthur@consilium-uk.com>
2020-05-08 10:13:42 +0100

    commit to master

---
+++ Scripts/BuildCleanup.ps1
@@ -0,0 +1,13 @@
+# Get ResourceID of the Image Template
+$resTemplateId = Get-AzResource -ResourceName $imageTemplateName -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ApiVersion "2019-05-01-preview"
+
+### Delete Image Template Artifact
+Remove-AzResource -ResourceId $resTemplateId.ResourceId -Force
+
+Remove-AzRoleAssignment -ObjectId $idenityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
+
+## remove definitions
+Remove-AzRoleDefinition -Name "$idenityNamePrincipalId" -Force -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
+
+## delete identity
+Remove-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName -Force
--- Scripts/Create-Image.ps1
+++ Scripts/Create-Image.ps1
@@ -1,7 +1,3 @@
 # Start image creation
 
 Invoke-AzResourceAction -ResourceName $imageTemplateName -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ApiVersion "2019-05-01-preview" -Action Run -Force
-
-# Check Status
-
-(Get-AzResource -ResourceGroupName RG_NEU_AzureImageBuilder -ResourceType Microsoft.VirtualMachineImages/imageTemplates -Name $ImageTemplateName).Properties.lastRunStatus
